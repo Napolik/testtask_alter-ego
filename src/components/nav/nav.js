c84a9f1fc3ce614b23f517613b7a73f44b809f56
RomanVelboi <wellboychik@gmail.com>
2021-11-26 14:48:57 +0200

    Added react jss for top menu

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,13 +1,32 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {createUseStyles} from 'react-jss';
+
+const useStyles = createUseStyles({
+    navItem: {
+        listStyleType: 'none',
+        margin: {
+            right: '20px',
+        },
+        '& a': {
+            fontSize: '20px',
+            color: 'black'
+          }      
+    },
+    navContainer: {
+        display: 'flex',
+        flexDirection: 'row'
+    }
+  })
   
 const Nav = () => {
+    const classes = useStyles();
     return(
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/news/">News</Link></li>
-                <li><Link to="/profile/">Profile</Link></li>
+            <ul className={classes.navContainer}>
+                <li className={classes.navItem}><Link to="/">Home</Link></li>
+                <li className={classes.navItem}><Link to="/news/">News</Link></li>
+                <li className={classes.navItem}><Link to="/profile/">Profile</Link></li>
             </ul> 
         </div>
     )
--- src/components/news/news.js
+++ src/components/news/news.js
@@ -3,6 +3,7 @@ import React from "react";
 const News = () => {
     return(
         <h2>News</h2>
+        
     )
 };
 
